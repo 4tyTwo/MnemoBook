36d6d51daffcdaeabf00c79e6e1b73dcff1c3887
Igor Toporkov <i.toporkov@rbkmoney.com>
2020-04-29 00:43:18 +0300

    Add icons for search bar

--- src/main/java/Controllers/HelpWindowController.java
+++ src/main/java/Controllers/HelpWindowController.java
@@ -1,6 +1,9 @@
 package Controllers;
 
 import Entities.InstructionList;
+import de.jensd.fx.glyphs.GlyphsBuilder;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import elements.MnemonicDescription;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -17,8 +20,27 @@ public class HelpWindowController {
                 map(MnemonicDescription::new).collect(Collectors.toList());
         instructionList.getItems().remove(0);
         instructionList.getItems().addAll(descriptions);
+
+        // Prettify?
+        buttonCloseSearch.setGraphic(GlyphsBuilder.create(FontAwesomeIconView.class).
+                glyph(FontAwesomeIcon.CLOSE).
+                build()
+        );
+        search.setGraphic(GlyphsBuilder.create(FontAwesomeIconView.class).
+                glyph(FontAwesomeIcon.SEARCH).
+                build()
+        );
+        buttonSearchUp.setGraphic(GlyphsBuilder.create(FontAwesomeIconView.class).
+                glyph(FontAwesomeIcon.SEARCH_PLUS).
+                build()
+        );
+        buttonSearchDown.setGraphic(GlyphsBuilder.create(FontAwesomeIconView.class).
+                glyph(FontAwesomeIcon.SEARCH_MINUS).
+                build()
+        );
     }
 
+    public Label search;
     public Button buttonCloseSearch;
     public TextField fieldSearch;
     public Button buttonSearchUp;
