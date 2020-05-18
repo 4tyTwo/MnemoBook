ddc6346734e080aba8d401c1bab5852c32a8462f
Igor Toporkov <i.toporkov@rbkmoney.com>
2020-05-18 23:07:26 +0300

    Sort InstructionList

--- src/main/java/Entities/InstructionList.java
+++ src/main/java/Entities/InstructionList.java
@@ -6,13 +6,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class InstructionList {
 
     public static final List<Instruction> INSTRUCTION_LIST = JSONConverter.convert(
             "src/main/resources/descriptions/mnemonics.json",
             Instructions.class
-    ).instructionList;
+    ).instructionList.stream().sorted().collect(Collectors.toList());
 
     private static class Instructions {
         @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
